14e59ebb15e7e87c1312db08fcd66c23d20f45da
Ngoguey42 <ngoguey@student.42.fr>
2015-12-20 18:44:18 +0100

    more yojson

--- srcs/YojsonParser/YojsonParser.ml
+++ srcs/YojsonParser/YojsonParser.ml
@@ -6,7 +6,7 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2015/12/20 16:25:13 by ngoguey           #+#    #+#             *)
-(*   Updated: 2015/12/20 18:00:37 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2015/12/20 18:41:58 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -15,13 +15,15 @@ let f = Yojson.Basic.from_file
 type json = [ `Assoc
 			| `List
 			| `String ]
+
 (* type json = [ `Assoc of (string * json) list *)
-(* 			(\* | `Bool of bool *\) *)
-(* 			(\* | `Float of float *\) *)
-(* 			(\* | `Int of int *\) *)
-(* 			| `List of json list *)
-(* 			(\* | `Null *\) *)
 (* 			| `String of string ] *)
+(* 			| `List of json list *)
+
+(* 			| `Bool of bool *)
+(* 			| `Float of float *)
+(* 			| `Int of int *)
+(* 			| `Null *)
 
 type funtype = Fun of string | NoFun
 
@@ -30,12 +32,14 @@ type handler =
   (* htable[string].funtype takes a (string * json) *)
   (* htable[string].handler applied on each assoc's rhs *)
 
-  | DynamicAssoc of funtype * handler
-  (* funtype takes a (string * json) list *)
+  | DynamicAssoc of funtype * funtype * handler
+  (* funtype#1 takes a (string * json) list *)
+  (* funtype#2 takes repeated (string * json) *)
   (* handler applied on each assoc's elements *)
 
-  | List of funtype * handler
-  (* funtype takes a json list *)
+  | List of funtype * funtype * handler
+  (* funtype#1 takes a json list *)
+  (* funtype#2 takes repeated json *)
   (* handler applied on each list's elements *)
 
   | String of funtype
@@ -44,32 +48,44 @@ type handler =
   | NoHandle
 
 
-let _hashtbl_of_array a =
+(* ~| Hashtbl constructor *)
+let (~|) : ('a * 'b) array -> ('a, 'b) Hashtbl.t = fun a ->
   let ht = Hashtbl.create (Array.length a) in
   Array.iter (fun (k, v) -> Hashtbl.add ht k v) a;
   ht
 
-let (~|) a = _hashtbl_of_array a
-let (~~) n f c = n, (f, c)
+(* ~~ Hashtbl entry constructor *)
+let (~~) : 'a -> 'b -> 'c -> 'a * ('b * 'c) = fun n f c ->
+  n, (f, c)
+
+(* ~> Fun constructor *)
+let (~>) : string -> funtype = fun fname ->
+  Fun fname
 
 let transition_handler =
   StaticAssoc
 	~| [|
-	  ~~ "read" NoFun @@ String (Fun "sv_trans_read")
-	; ~~ "to_state" NoFun @@ String (Fun "sv_trans_tostate")
-	; ~~ "write" NoFun @@ String (Fun "sv_trans_write")
-	; ~~ "action" NoFun @@ String (Fun "sv_trans_action")
+	  ~~ "read" NoFun @@ String ~>"sv_trans_read"
+	; ~~ "to_state" NoFun @@ String ~>"sv_trans_tostate"
+	; ~~ "write" NoFun @@ String ~>"sv_trans_write"
+	; ~~ "action" NoFun @@ String ~>"sv_trans_action"
 	|]
+
+let transitions_handler =
+  DynamicAssoc (~>"ck_trans_count", ~>"sv_trans_name",
+				List (~>"ck_substransitions_count", NoFun, transition_handler))
 
 let file_handler =
   StaticAssoc
 	~| [|
-	  ~~ "name" NoFun @@ String (Fun "sv_name")
-	; ~~ "alphabet" NoFun @@ List (NoFun, String (Fun "sv_letter"))
-	; ~~ "blank" NoFun @@ String (Fun "sv_blank")
-	; ~~ "states" NoFun @@ List (NoFun, String (Fun "sv_state"))
-	; ~~ "initial" NoFun @@ String (Fun "sv_initial")
-	; ~~ "finals" NoFun @@ List (NoFun, String (Fun "sv_final"))
-	; ~~ "transitions" NoFun
-	  @@ DynamicAssoc ((Fun "sv_trans_name"), List (NoFun, transition_handler))
+	  ~~ "name" NoFun @@ String ~>"sv_name"
+	; ~~ "blank" NoFun @@ String ~>"sv_blank"
+	; ~~ "initial" NoFun @@ String ~>"sv_initial"
+	; ~~ "alphabet" NoFun
+	  @@ List (~>"ck_letters_count", NoFun, String ~>"sv_letter")
+	; ~~ "states" NoFun
+	  @@ List (~>"ck_states_count", NoFun, String ~>"sv_state")
+	; ~~ "finals" NoFun
+	  @@ List (~>"ck_finals_count", NoFun, String ~>"sv_final")
+	; ~~ "transitions" NoFun @@ transitions_handler
 	|]
