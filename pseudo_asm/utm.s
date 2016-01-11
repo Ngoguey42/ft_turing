873dcb891517188973ce46a6dbef31f70edfb485
Ngoguey42 <ngoguey@student.42.fr>
2016-01-11 15:44:05 +0100

    wrote state_nextl and state_nextr

--- pseudo_asm/utm.s
+++ pseudo_asm/utm.s
@@ -6,7 +6,7 @@
 ;    By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2016/01/11 14:16:30 by ngoguey           #+#    #+#              ;
-;    Updated: 2016/01/11 14:54:59 by ngoguey          ###   ########.fr        ;
+;    Updated: 2016/01/11 15:43:15 by ngoguey          ###   ########.fr        ;
 ;                                                                              ;
 ;******************************************************************************;
 
@@ -44,3 +44,66 @@
 
 ; TapeChars ::= EPSILON | Flags2 AlphabetChar TapeChars  		;Can be empty
 ; Tape ::= = TapeChars
+
+	name"utm.s"
+	alphabet[uyn+-=01ab.]
+	blank[.]
+
+main:
+	__		[ANY]			L		call state_nextl
+	__		[ANY]			L		call state_nextr
+	__		[ANY]			L		halt
+
+; STATES/TRANSITIONS RIGHT MOVES
+trans_nextl:
+	__		[uyn]			L		ni ; (trans rbegin)
+	|		[+]				E		ret; (transs lend) (state lbegin)
+	__		[01]			L		ni
+	__		[ANY]			L		ni
+	__		[01]			L		ni
+	__		[ANY]			L		ni
+	__		[01]			L		ni
+	__		[=]				L		ni
+	__		[01ab]			L		rep
+	|		[-]				L		ret ;leaves head in a similar configuration
+
+lskip_any_trans:
+	__		[+]				E		ret ; (transs lend) (state lbegin)
+	|		[uyn]			E		call trans_nextl ; (trans rbegin)
+	__		[ANY]			E		jmp lskip_any_trans
+
+state_nextl:
+	__		[uyn]			L		ni ; (state rbegin)
+	|		[.]				E		ret; (states lend)
+	__		[01]			L		ni
+	__		[=]				L		ni
+	__		[01ab]			L		rep
+	|		[=]				L		call lskip_any_trans
+	__		[+]				L		ret ;leaves head in a similar configuration
+
+; STATES/TRANSITIONS LEFT MOVES
+trans_nextr:
+	__		[-]				R		ni ; (trans lbegin)
+	|		[=]				E		ret; (transs rend)
+	__		[01ab]			R		rep
+	|		[=]				R		ni
+	__		[01]			R		ni
+	__		[ANY]			R		ni
+	__		[01]			R		ni
+	__		[ANY]			R		ni
+	__		[01]			R		ni
+	__		[uyn]			R		ret ;leaves head in a similar configuration
+
+rskip_any_trans:
+	__		[=]				E		ret ; (transs rend)
+	|		[-]				E		call trans_nextr ; (trans lbegin)
+	__		[ANY]			E		jmp rskip_any_trans
+
+state_nextr:
+	__		[+]				R		call rskip_any_trans ;(transs lend) (state lbegin)
+	|		[=]				E		ret; (states rend)
+	__		[=]				R		ni
+	__		[01ab]			R		rep
+	|		[=]				R		ni
+	__		[01]			R		ni
+	__		[uyn]			R		ret;leaves head in a similar configuration
