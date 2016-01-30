64c6e03e06c23116de2d40500697af08cfc09fd3
Ngoguey42 <ngoguey@student.42.fr>
2016-01-30 19:39:28 +0100

    back to strings with Complexity.ml

--- srcs/Complexity.ml
+++ srcs/Complexity.ml
@@ -6,11 +6,11 @@
 (*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/01/30 16:11:00 by ngoguey           #+#    #+#             *)
-(*   Updated: 2016/01/30 19:29:01 by ngoguey          ###   ########.fr       *)
+(*   Updated: 2016/01/30 19:38:20 by ngoguey          ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
-let maxstrlen = 56
+let maxstrlen = 10
 
 module PD = ProgramData
 module CA = Core.Core_array
@@ -22,12 +22,12 @@ let i = ref 0
 let rec loop results db alpha str strlen =
   incr i;
   (* Printf.eprintf "\"%s\"\n%!" (Bytes.to_string str); *)
-  let membership = Verifier.verify db (Bytes.to_string str) in
+  let membership = Verifier.verify db str in
   (match membership with
    | Verifier.Member steps ->
 	  let (prev, _) = CA.unsafe_get results strlen in
 	  if steps > prev
-	  then CA.unsafe_set results strlen (steps, (Bytes.to_string str));
+	  then CA.unsafe_set results strlen (steps, str);
   (* Printf.eprintf "\"%s\" %d \n%!" str steps *)
 	  ()
    | _ -> ()
@@ -36,11 +36,11 @@ let rec loop results db alpha str strlen =
   | true, Verifier.Member steps
   | true, Verifier.Not steps when steps >= strlen ->
 	 CA.iter alpha ~f:(fun c ->
-			   Bytes.set str strlen c;
+			   (* Bytes.set str strlen c; *)
 			   loop results db alpha
-					str
+					(str ^ (String.make 1 c))
 					(strlen + 1));
-	 Bytes.set str strlen db.PD.blank
+	 (* Bytes.set str strlen db.PD.blank *)
   (* Printf.eprintf "Droping fail: %s\n%!" str; *)
   | _, _ -> ()
 
@@ -58,7 +58,7 @@ let tot alphalen =
 let compute db =
   let alpha = CA.filter_map db.PD.alphabet ~f:(alpha_filter db) in
   let results = CA.create ~len:(maxstrlen + 1) (~-1, "") in
-  loop results db alpha (Bytes.make (maxstrlen + 1) db.PD.blank) 0;
+  loop results db alpha "" 0;
   CA.iteri results ~f:(fun i (v, str) -> Printf.eprintf "%3d\t%3d \"%s\"\n%!" i v str);
 
   (* let rec fact i acc = match i with 0 -> acc | _ -> fact (i - 1) (acc * i) in *)
